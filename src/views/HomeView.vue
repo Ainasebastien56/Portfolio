9d9f3f8209a0c622ff32eea27bfdf238e5ab0db3
Sébastien Randriamanana <sebastienrandriamanana3@gmail.com>
2024-10-04 22:13:04 +0300

    last commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,19 @@
 <template>
-  <div style="background-color: #E3E3E3;" class="relative  w-full h-screen flex flex-col md:flex-row lg:flex-row items-center mt-20 md:mt-0">
+  <div style="background-color: #E3E3E3;" class="relative  w-full h-screen flex flex-col md:flex-row lg:flex-row items-center mt-20 md:mt-0 lg:mt-0">
     <div class="absolute top-36 md:top-0 lg:top-0 left-0 flex  flex-col h-full w-full justify-center items-center relative">
-      <iframe  class="w-full h-full my-5 md:my-0 lg:my-0 " src="https://my.spline.design/nexbotrobotcharacterconcept-f038210e78ac1b01ca5a8464d25f0404/" frameborder="0"  allow="autoplay; fullscreen">
+      <iframe  class="w-full h-full my-5 md:my-0 lg:my-0 " src="https://my.spline.design/nexbotrobotcharacterconcept-f038210e78ac1b01ca5a8464d25f0404/" frameborder="0"  allow="autoplay">
       </iframe>
       <p style="background-color: #E3E3E3;" class="absolute bottom-0  w-full  md:bottom-5 right-0  md:w-44 rounded py-10 md:py-10 "></p>
     </div>
     <button class=" block md:hidden bg-blue-600 text-white rounded w-32 mt-20 py-2 transform hover:-translate-y-1 hover:scale-110 hover:ring-2 ring-blue shadow-lg transition-all duration 500 ease-in-out">Let's start<font-awesome-icon class="ml-2" icon="arrow-right"/> </button>
     <div class="absolute left-0  transform top-20 -translate-y-auto md:top-1/2 lg:top-1/2  md:-translate-y-1/2 lg:-translate-y-1/2 text-justify w-full h-auto md:w-2/5 text-black z-10  flex flex-col items-center justify-center text-justify px-5 md:pl-10 md:pr-20">
-        <h1 class="text-3xl md:text-5xl lg:text-5xl font-bold mb-5 ">Web Developer</h1>
+        <h1 class="text-3xl md:text-5xl lg:text-5xl md:text-white font-bold mb-5 ">Web Developer</h1>
       <p class="mb-5 font-semibold text-md md:text-lg lg:text-lg text-gray-600">I'm a web developper specializing in responsive and intuitive front-end
         experiences. While I focus on front-end technologies, I also have some experience
         with back-end to support full-stack collaboration.
       </p>
         <button class=" hidden md:block bg-blue-600 text-white rounded w-32 mb-5 py-2 transform hover:-translate-y-1 hover:scale-110 hover:ring-2 ring-blue shadow-lg transition-all duration 500 ease-in-out">Let's start<font-awesome-icon class="ml-2" icon="arrow-right"/> </button>
     </div>
-
-    
-
     </div>
 </template>
 
